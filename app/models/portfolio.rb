54dbff0cda8293b255b445573b2f6e14ea6692c2
Samir Ben sadi <samirbensadi@gmail.com>
2017-08-02 20:26:27 +0200

    implemented custom scopes

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,15 @@
 class Portfolio < ApplicationRecord
 	validates_presence_of :title, :body, :main_image, :thumb_image 
 
+
+	def self.angular
+		where(subtitle: "Angular")
+	end
+
+	def self.rails
+		where(subtitle: "Ruby on Rails")
+	end
+	# an other way to write this up.
+	# scope :ruby_on_rails_portfolio_items, -> {where(subtitle: "Ruby on Rails")}
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+3.times do |topic|
+	Topic.create!(
+		title: "Topic #{topic}"
+		)
+end
+
+
 10.times do |blog|
     Blog.create!(
         title: "My Blog Post #{blog}", 
-        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis varius mi libero, id bibendum metus laoreet nec. Etiam commodo tempor risus, vitae mattis erat faucibus nec. Nulla bibendum sapien vitae libero congue, at elementum metus finibus. Donec rutrum rhoncus tellus vel sollicitudin. Curabitur elementum at sem eget pretium. Aenean varius risus ac libero elementum, et blandit lectus tempor. Ut ultrices erat quis dolor pretium mattis. Sed et odio id orci egestas suscipit."
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis varius mi libero, id bibendum metus laoreet nec. Etiam commodo tempor risus, vitae mattis erat faucibus nec. Nulla bibendum sapien vitae libero congue, at elementum metus finibus. Donec rutrum rhoncus tellus vel sollicitudin. Curabitur elementum at sem eget pretium. Aenean varius risus ac libero elementum, et blandit lectus tempor. Ut ultrices erat quis dolor pretium mattis. Sed et odio id orci egestas suscipit.",
+        topic_id: Topic.last.id 
     )
 end
 
@@ -28,7 +37,17 @@ puts "5 skills created"
 6.times do |portfolio_item|
 	Portfolio.create!(
 		title: "Portfolio title: #{portfolio_item}", 
-		subtitle: "My great service", 
+		subtitle: "Ruby on Rails", 
+		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vulputate sollicitudin luctus. Nam sit amet tempor est. Morbi in urna mollis, aliquam arcu ac, posuere tortor. Nulla ut lorem sem. Proin vel condimentum mauris. Nam efficitur sed orci nec vehicula. Sed dignissim mollis risus, vitae pellentesque enim ultrices a. Duis finibus orci sit amet tortor interdum, nec egestas neque eleifend. Pellentesque tortor augue, dictum vel bibendum sed, egestas nec erat. Vivamus mollis magna eu tempus tincidunt. Vivamus pellentesque sem justo, ut tincidunt elit eleifend nec. Mauris aliquet eros sit amet odio rutrum condimentum." , 
+		main_image: "http://via.placeholder.com/600x400", 
+		thumb_image: "http://via.placeholder.com/350x200" 
+		)
+end
+
+1.times do |portfolio_item|
+	Portfolio.create!(
+		title: "Portfolio title: #{portfolio_item}", 
+		subtitle: "Angular", 
 		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vulputate sollicitudin luctus. Nam sit amet tempor est. Morbi in urna mollis, aliquam arcu ac, posuere tortor. Nulla ut lorem sem. Proin vel condimentum mauris. Nam efficitur sed orci nec vehicula. Sed dignissim mollis risus, vitae pellentesque enim ultrices a. Duis finibus orci sit amet tortor interdum, nec egestas neque eleifend. Pellentesque tortor augue, dictum vel bibendum sed, egestas nec erat. Vivamus mollis magna eu tempus tincidunt. Vivamus pellentesque sem justo, ut tincidunt elit eleifend nec. Mauris aliquet eros sit amet odio rutrum condimentum." , 
 		main_image: "http://via.placeholder.com/600x400", 
 		thumb_image: "http://via.placeholder.com/350x200" 
